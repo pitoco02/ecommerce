0944ab545a009b2330314c711f11513d63a13ecb
Paulo Borges <paulocesarbr02@outlook.com>
2020-05-26 23:27:59 -0300

    Site - Produtos 110

--- admin-categories.php
+++ admin-categories.php
@@ -1,5 +1,5 @@
 <?php 
-
+use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 use \Hcode\Model\Category;
--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -15,6 +15,17 @@ class Product extends Model{
 		return $sql->select("SELECT * FROM tb_products ORDER BY desproduct");
 	}
 
+	public static function checkList($list){
+		foreach ($list as &$row) {
+			$p = new Product();
+			$p->setData($row);
+			$row= $p->getValues();
+		}
+
+		return $list;
+	}
+
+
 	public function save()
 	{
 		$sql = new Sql();
@@ -122,8 +133,8 @@ class Product extends Model{
 			$this->getidproduct().".jpg";
 
 		imagejpeg($image, $dist);
-
 		imagedestroy($image);
+
 
 		$this->checkPhoto();
 
