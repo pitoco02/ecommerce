9fe4f9f9d372485b2df6f8511fa55214e96c9076
Paulo Borges <paulocesarbr02@outlook.com>
2020-07-19 19:37:45 -0300

    Carrinho de compras 116

--- site.php
+++ site.php
@@ -3,7 +3,7 @@
 use \Hcode\Page;
 use \Hcode\Model\Product;
 use \Hcode\Model\Category;
-
+use \Hcode\Model\Cart;
 
 $app->get('/', function() {
 
@@ -62,6 +62,17 @@ $app->get("/products/:desurl",function($desurl){
 });
 
 
+$app->get("/cart", function(){
+
+	$cart = Cart::getFromSession();
+
+	$page = new Page();
+
+	$page->setTpl("cart");
+
+
+
+});
 
 
 
