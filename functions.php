543cdc866ccbb5a3a31bd059151b06e5ed432d2d
Paulo Borges <paulocesarbr02@outlook.com>
2020-07-29 17:19:44 -0300

    Esqueci senha Site 121

--- functions.php
+++ functions.php
@@ -2,9 +2,14 @@
 
 use \Hcode\Model\User;
 
-	function formatPrice(float $vlprice){
-		return number_format($vlprice, 2, "," , ".");
-	}
+function formatPrice($vlprice)
+{
+
+	if (!$vlprice > 0) $vlprice = 0;
+
+	return number_format($vlprice, 2, ",", ".");
+
+}
 
 function formatDate($date)
 {
