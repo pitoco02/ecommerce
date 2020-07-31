ad8fd84290d5f94be1dad7678ee411aaba06301f
Paulo Borges <paulocesarbr02@outlook.com>
2020-07-30 21:25:32 -0300

    Admin produtos Busca e paginação 130

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -6,29 +6,37 @@ use \Hcode\DB\Sql;
 use \Hcode\Model;
 use \Hcode\Mailer;
 
-class Product extends Model{
-
+class Product extends Model {
 
 	public static function listAll()
 	{
+
 		$sql = new Sql();
+
 		return $sql->select("SELECT * FROM tb_products ORDER BY desproduct");
+
 	}
 
-	public static function checkList($list){
+	public static function checkList($list)
+	{
+
 		foreach ($list as &$row) {
+			
 			$p = new Product();
 			$p->setData($row);
-			$row= $p->getValues();
+			$row = $p->getValues();
+
 		}
 
 		return $list;
-	}
 
+	}
 
 	public function save()
 	{
+
 		$sql = new Sql();
+
 		$results = $sql->select("CALL sp_products_save(:idproduct, :desproduct, :vlprice, :vlwidth, :vlheight, :vllength, :vlweight, :desurl)", array(
 			":idproduct"=>$this->getidproduct(),
 			":desproduct"=>$this->getdesproduct(),
@@ -42,68 +50,71 @@ class Product extends Model{
 
 		$this->setData($results[0]);
 
-
 	}
 
 	public function get($idproduct)
 	{
 
 		$sql = new Sql();
-		$results = $sql->select("SELECT * FROM tb_products WHERE idproduct = :idproduct",[
-			":idproduct"=>$idproduct
+
+		$results = $sql->select("SELECT * FROM tb_products WHERE idproduct = :idproduct", [
+			':idproduct'=>$idproduct
 		]);
 
 		$this->setData($results[0]);
 
-
 	}
 
-
-
-	public function  delete()
+	public function delete()
 	{
+
 		$sql = new Sql();
+
 		$sql->query("DELETE FROM tb_products WHERE idproduct = :idproduct", [
-			":idproduct"=>$this->getidproduct()
+
+			':idproduct'=>$this->getidproduct()
+			
 		]);
 
 	}
 
 	public function checkPhoto()
 	{
-		if(file_exists(
-			$_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR .
-			"res" . DIRECTORY_SEPARATOR .
+
+		if (file_exists(
+			$_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 
+			"res" . DIRECTORY_SEPARATOR . 
 			"site" . DIRECTORY_SEPARATOR . 
-			"img" . DIRECTORY_SEPARATOR .
-			"products" . DIRECTORY_SEPARATOR .
-			$this->getidproduct().".jpg" 
-		)) {
-			$url = "/res/site/img/products/".$this->getidproduct().".jpg";
+			"img" . DIRECTORY_SEPARATOR . 
+			"products" . DIRECTORY_SEPARATOR . 
+			$this->getidproduct() . ".jpg"
+			)) {
 
-		} else{
-		 $url = "/res/site/img/product.jpg";
+			$url = "/res/site/img/products/" . $this->getidproduct() . ".jpg";
+
+		} else {
+
+			$url = "/res/site/img/product.jpg";
+
 		}
-
 
 		return $this->setdesphoto($url);
 
 	}
 
-
-
 	public function getValues()
 	{
+
 		$this->checkPhoto();
 
 		$values = parent::getValues();
 
 		return $values;
+
 	}
 
-
-
-	public function setPhoto($file){
+	public function setPhoto($file)
+	{
 
 		$extension = explode('.', $file['name']);
 		$extension = end($extension);
@@ -121,20 +132,20 @@ class Product extends Model{
 
 			case "png":
 			$image = imagecreatefrompng($file["tmp_name"]);
-			break;	
+			break;
 
 		}
 
-		$dist = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR .
-			"res" . DIRECTORY_SEPARATOR .
+		$dist = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 
+			"res" . DIRECTORY_SEPARATOR . 
 			"site" . DIRECTORY_SEPARATOR . 
-			"img" . DIRECTORY_SEPARATOR .
-			"products" . DIRECTORY_SEPARATOR .
-			$this->getidproduct().".jpg";
+			"img" . DIRECTORY_SEPARATOR . 
+			"products" . DIRECTORY_SEPARATOR . 
+			$this->getidproduct() . ".jpg";
 
 		imagejpeg($image, $dist);
-		imagedestroy($image);
 
+		imagedestroy($image);
 
 		$this->checkPhoto();
 
@@ -142,12 +153,13 @@ class Product extends Model{
 
 
 
-
 	public function getFromURL($desurl)
 	{
+
 		$sql = new Sql();
-		$rows =  $sql->select("SELECT * FROM tb_products WHERE desurl = :desurl LIMIT 1",[
-			'desurl'=>$desurl
+
+		$rows = $sql->select("SELECT * FROM tb_products WHERE desurl = :desurl LIMIT 1", [
+			':desurl'=>$desurl
 		]);
 
 		$this->setData($rows[0]);
@@ -156,22 +168,72 @@ class Product extends Model{
 
 	public function getCategories()
 	{
+
 		$sql = new Sql();
+
 		return $sql->select("
 			SELECT * FROM tb_categories a INNER JOIN tb_productscategories b ON a.idcategory = b.idcategory WHERE b.idproduct = :idproduct
-			",[
-				'idproduct'=>$this->getidproduct()
-			]);
+		", [
+
+			':idproduct'=>$this->getidproduct()
+		]);
+
+	}
+
+	public static function getPage($page = 1, $itemsPerPage = 10)
+	{
+
+		$start = ($page - 1) * $itemsPerPage;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_products 
+			ORDER BY desproduct
+			LIMIT $start, $itemsPerPage;
+		");
+
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return [
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
+
 	}
 
 
+
+
+	public static function getPageSearch($search, $page = 1, $itemsPerPage = 10)
+	{
+
+		$start = ($page - 1) * $itemsPerPage;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_products 
+			WHERE desproduct LIKE :search
+			ORDER BY desproduct
+			LIMIT $start, $itemsPerPage;
+		", [
+			':search'=>'%'.$search.'%'
+		]);
+
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return [
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
+
+	}
+
 }
-
-
-
-
-
-
-
 
  ?>
