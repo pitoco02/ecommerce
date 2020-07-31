72bb4a2369cd8650a27503344b49757baf521f9b
Paulo Borges <paulocesarbr02@outlook.com>
2020-07-30 21:18:36 -0300

    Categorias Busca e paginação 129

--- admin-categories.php
+++ admin-categories.php
@@ -4,17 +4,45 @@ use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 use \Hcode\Model\Category;
 use \Hcode\Model\Product;
-
 $app->get("/admin/categories", function(){
 
 	User::verifyLogin();
 
-	$categories = Category::listAll();
+	$search = (isset($_GET['search'])) ? $_GET['search'] : "";
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
+	if ($search != '') {
+
+		$pagination = Category::getPageSearch($search, $page);
+
+	} else {
+
+		$pagination = Category::getPage($page);
+
+	}
+
+	$pages = [];
+
+	for ($x = 0; $x < $pagination['pages']; $x++)
+	{
+
+		array_push($pages, [
+			'href'=>'/admin/categories?'.http_build_query([
+				'page'=>$x+1,
+				'search'=>$search
+			]),
+			'text'=>$x+1
+		]);
+
+	}
 
 	$page = new PageAdmin();
-	$page->setTpl("categories",[
-		'categories'=>$categories
-	]);
+
+	$page->setTpl("categories", [
+		"categories"=>$pagination['data'],
+		"search"=>$search,
+		"pages"=>$pages
+	]);	
 
 
 });
--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -6,18 +6,22 @@ use \Hcode\DB\Sql;
 use \Hcode\Model;
 use \Hcode\Mailer;
 
-class Category extends Model{
-
+class Category extends Model {
 
 	public static function listAll()
 	{
+
 		$sql = new Sql();
+
 		return $sql->select("SELECT * FROM tb_categories ORDER BY descategory");
+
 	}
 
 	public function save()
 	{
+
 		$sql = new Sql();
+
 		$results = $sql->select("CALL sp_categories_save(:idcategory, :descategory)", array(
 			":idcategory"=>$this->getidcategory(),
 			":descategory"=>$this->getdescategory()
@@ -33,67 +37,69 @@ class Category extends Model{
 	{
 
 		$sql = new Sql();
-		$results = $sql->select("SELECT * FROM tb_categories WHERE idcategory = :idcategory",[
-			":idcategory"=>$idcategory
+
+		$results = $sql->select("SELECT * FROM tb_categories WHERE idcategory = :idcategory", [
+			':idcategory'=>$idcategory
 		]);
 
 		$this->setData($results[0]);
 
-
 	}
 
-
-
-	public function  delete()
+	public function delete()
 	{
+
 		$sql = new Sql();
+
 		$sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory", [
-			":idcategory"=>$this->getidcategory()
+			':idcategory'=>$this->getidcategory()
 		]);
 
 		Category::updateFile();
-	}
 
+	}
 
 	public static function updateFile()
 	{
 
 		$categories = Category::listAll();
-		$html = [];
-		foreach ($categories as $row) {
-			array_push($html, '<li><a href="/categories/'.$row["idcategory"].'">'.$row["descategory"].'</a></li>');
-		}
-		file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR ."views".DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
-	}
 
+		$html = [];
+
+		foreach ($categories as $row) {
+			array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+		}
+
+		file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
+
+	}
 
 	public function getProducts($related = true)
 	{
+
 		$sql = new Sql();
 
-		if($related === true){
+		if ($related === true) {
 
 			return $sql->select("
-				SELECT * FROM tb_products where idproduct in(
+				SELECT * FROM tb_products WHERE idproduct IN(
 					SELECT a.idproduct
 					FROM tb_products a
 					INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
 					WHERE b.idcategory = :idcategory
-
 				);
 			", [
 				':idcategory'=>$this->getidcategory()
 			]);
 
-		}else {
+		} else {
 
 			return $sql->select("
-				SELECT * FROM tb_products where idproduct NOT in(
+				SELECT * FROM tb_products WHERE idproduct NOT IN(
 					SELECT a.idproduct
 					FROM tb_products a
 					INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
 					WHERE b.idcategory = :idcategory
-
 				);
 			", [
 				':idcategory'=>$this->getidcategory()
@@ -101,12 +107,9 @@ class Category extends Model{
 
 		}
 
-
-
 	}
 
-
-public function getProductsPage($page = 1, $itemsPerPage = 8)
+	public function getProductsPage($page = 1, $itemsPerPage = 8)
 	{
 
 		$start = ($page - 1) * $itemsPerPage;
@@ -134,55 +137,81 @@ public function getProductsPage($page = 1, $itemsPerPage = 8)
 
 	}
 
-
-
 	public function addProduct(Product $product)
 	{
+
 		$sql = new Sql();
 
-		$sql->query("INSERT INTO tb_productscategories (idcategory, idproduct) VALUES (:idcategory, :idproduct)", [
+		$sql->query("INSERT INTO tb_productscategories (idcategory, idproduct) VALUES(:idcategory, :idproduct)", [
 			':idcategory'=>$this->getidcategory(),
 			':idproduct'=>$product->getidproduct()
-
 		]);
 
 	}
 
-
 	public function removeProduct(Product $product)
 	{
+
 		$sql = new Sql();
 
 		$sql->query("DELETE FROM tb_productscategories WHERE idcategory = :idcategory AND idproduct = :idproduct", [
 			':idcategory'=>$this->getidcategory(),
 			':idproduct'=>$product->getidproduct()
-
 		]);
 
 	}
+			
+	public static function getPage($page = 1, $itemsPerPage = 10)
+	{
 
+		$start = ($page - 1) * $itemsPerPage;
 
+		$sql = new Sql();
 
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_categories 
+			ORDER BY descategory
+			LIMIT $start, $itemsPerPage;
+		");
 
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
 
+		return [
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
 
+	}
 
+	public static function getPageSearch($search, $page = 1, $itemsPerPage = 10)
+	{
 
+		$start = ($page - 1) * $itemsPerPage;
 
+		$sql = new Sql();
 
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_categories 
+			WHERE descategory LIKE :search
+			ORDER BY descategory
+			LIMIT $start, $itemsPerPage;
+		", [
+			':search'=>'%'.$search.'%'
+		]);
 
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
 
+		return [
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
 
-
-
+	}
 
 }
-
-
-
-
-
-
-
 
  ?>
